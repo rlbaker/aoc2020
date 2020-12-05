66c8b570dd0ff25f18f217b0616cc110c3f37fe1
Ryan Baker <mail@rlbaker.net>
2020-12-05 01:14:09 -0800

    cleanup

--- src/day5.ml
+++ src/day5.ml
@@ -6,17 +6,24 @@ let to_bin = function
   | 'B' | 'R' -> '1'
   | _ -> assert false
 
-let day5_1 seats =
+let max seats =
   match List.max_elt seats ~compare:Int.compare with
   | Some max -> max
   | _ -> assert false
 
-let day5_2 seats =
-  let valid_seats = Set.Poly.of_list (List.range 46 992)
-  and filled_seats = Set.Poly.of_list seats in
-  Set.diff valid_seats filled_seats
+let min seats =
+  match List.min_elt seats ~compare:Int.compare with
+  | Some max -> max
+  | _ -> assert false
 
 let seat_id line = Int.of_string ("0b" ^ String.map line ~f:to_bin)
+
+let day5_1 = max
+
+let day5_2 seats =
+  let valid_seats = Set.Poly.of_list (List.range (min seats) (max seats))
+  and filled_seats = Set.Poly.of_list seats in
+  Set.diff valid_seats filled_seats
 
 let () =
   let input = In_channel.input_lines In_channel.stdin
