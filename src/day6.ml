1f00525d0f355e31297306a48d24f92abffab608
Ryan Baker <mail@rlbaker.net>
2020-12-06 11:07:23 -0800

    day 6

--- src/day6.ml
+++ src/day6.ml
@@ -7,16 +7,16 @@ let len f = function
 
 let sum f = List.sum (module Int) ~f
 
+let day6_1 = len Set.union |> sum
+
+let day6_2 = len Set.inter |> sum
+
 let read_input =
   let to_set s = String.to_list s |> Set.of_list (module Char) in
   let to_sets s = String.split_lines s |> List.map ~f:to_set in
   In_channel.input_all In_channel.stdin
   |> String.substr_replace_all ~pattern:"\n\n" ~with_:"|"
   |> String.split ~on:'|' |> List.map ~f:to_sets
-
-let day6_1 = len Set.union |> sum
-
-let day6_2 = len Set.inter |> sum
 
 let () =
   let input = read_input
